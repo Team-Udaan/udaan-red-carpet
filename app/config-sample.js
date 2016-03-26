530163adb24c03e42f37988dcb3c57f812599259
Dhruvin Gandhi <dhruvin2910@gmail.com>
2016-03-26 22:37:01 +0530

    Add TEST mode and BYPASS_LOGIN to config-sample.js

--- app/config-sample.js
+++ app/config-sample.js
@@ -3,5 +3,9 @@
 // Set DATA_URL
 var URC_DATA_URL = '';
 
+// Test mode and Bypass login
+var TEST = false;
+var BYPASS_LOGIN = false;
+
 // Load data.js
 document.write('<script src="' + URC_DATA_URL + '"></script>');
